e323c50a74ee51d0f3fa078af6666f079030ad26
runoob <2631731283@qq.com>
2018-09-12 11:45:43 +0800

    First homework

---
+++ homework1/program1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace program1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s;
+            double num1, num2;
+            Console.Write("Please input num1:");
+            s = Console.ReadLine();
+            num1 = Double.Parse(s);
+            Console.Write("Please input num2:");
+            s = Console.ReadLine();
+            num2 = Double.Parse(s);
+            Console.WriteLine("The multiplication of num1 and num2 is:" + (num1 * num2));
+        }
+    }
+}
---
+++ homework1/program2/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace program2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string s = textBox1.Text;
+            double num1 = double.Parse(s);
+            s = textBox2.Text;
+            double num2 = double.Parse(s);
+            label1.Text = "num1与num2的积是：" + num1 * num2;
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
